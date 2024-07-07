fb856adbe0c32db805610c02b6ba2cdd8b35b329
Adriano Miranda <adrjmiranda@gmail.com>
2024-07-07 13:12:34 -0300

    feat: created class responsible for responses and started routing class

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -4,4 +4,45 @@ namespace Src\Http;
 
 class Response
 {
+  private array $headers;
+  private string $contentType;
+  private int $httpCode;
+
+  private function setContentType(string $contentType)
+  {
+    $this->contentType = $contentType;
+    $this->setHeaders('Content-Type', $contentType);
+  }
+
+  private function setHeaders(string $key, mixed $value)
+  {
+    $this->headers[$key] = $value;
+  }
+
+  private function sendHeaders()
+  {
+    http_response_code($this->httpCode);
+
+    foreach ($this->headers as $key => $value) {
+      header($key . ': ' . $value);
+    }
+  }
+
+  public function send(mixed $content, int $httpCode, string $contentType = 'text/html')
+  {
+    $this->setContentType($contentType);
+    $this->httpCode = $httpCode;
+
+    $this->sendHeaders();
+
+    switch ($this->contentType) {
+      case 'text/html':
+        echo $content;
+        exit;
+
+      case 'application/json':
+        echo json_encode($content, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+        exit;
+    }
+  }
 }
--- src/Http/Router.php
+++ src/Http/Router.php
@@ -4,4 +4,23 @@ namespace Src\Http;
 
 class Router
 {
+  const PARAMS_PATTERN = '/\{([^}]*)\}/';
+
+  private string $baseUrl;
+  private Request $request;
+  private Response $response;
+  private array $staticRoutes;
+  private array $dynamicRoutes;
+  private array $params;
+
+  public function __construct()
+  {
+    $this->request = new Request;
+    $this->response = new Response;
+
+    $this->staticRoutes = [];
+    $this->dynamicRoutes = [];
+
+    $this->params = [];
+  }
 }
