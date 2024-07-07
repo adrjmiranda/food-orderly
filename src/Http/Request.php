668e173d63b566d5bb295316e6ae50a328ed837b
Adriano Miranda <adrjmiranda@gmail.com>
2024-07-07 12:09:00 -0300

    chore: created class responsible for requests

---
+++ src/Http/Request.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Src\Http;
+
+class Request
+{
+  private string $uri;
+  private array $headers;
+  private string $httpMethod;
+  private array $queryParams;
+  private array $postVars;
+  private array $files;
+
+  public function __construct()
+  {
+    $this->headers = $this->get_all_headers();
+    $this->queryParams = $_GET ?? [];
+    $this->postVars = $_POST ?? [];
+    $this->uri = $this->getOnlyUri();
+    $this->files = $_FILES ?? [];
+    $this->httpMethod = $_SERVER['REQUEST_METHOD'] ?? '';
+  }
+
+  private function get_all_headers(): array
+  {
+    $headers = [];
+    foreach ($_SERVER as $key => $value) {
+      if (strpos($key, 'HTTP_') === 0) {
+        $headerName = str_replace('_', '-', strtolower(substr($key, 5)));
+        $headers[$headerName] = $value;
+      }
+    }
+
+    return $headers;
+  }
+
+  private function getOnlyUri(): string
+  {
+    $url = parse_url($_SERVER['REQUEST_URI']);
+    return $url['path'];
+  }
+
+  public function getUri(): string
+  {
+    return $this->uri;
+  }
+
+  public function getHeaders(): array
+  {
+    return $this->headers;
+  }
+
+  public function getHttpMethod(): string
+  {
+    return $this->httpMethod;
+  }
+
+  public function getQueryParams(): array
+  {
+    return $this->queryParams;
+  }
+
+  public function getPostVars(): array
+  {
+    return $this->postVars;
+  }
+
+  public function getFile(string $name)
+  {
+    return $this->files['name'] ?? null;
+  }
+}
