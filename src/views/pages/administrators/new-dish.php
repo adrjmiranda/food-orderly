f9c7f241112f531f6c91001aa424cec31a50f7ff
Adriano Miranda <adrjmiranda@gmail.com>
2024-07-10 10:44:13 -0300

    chore: new dish creation form created

--- src/views/pages/administrators/new-dish.php
+++ src/views/pages/administrators/new-dish.php
@@ -14,6 +14,49 @@ $this->extend('administrators/master', [
 
     <?php require_once __DIR__ . "/../../components/administrators/session-bar.php"; ?>
 
-    <!-- TODO: create form -->
+    <!-- Data Form -->
+
+    <form action="#" id="data-form">
+      <!-- TODO: fill csrf value -->
+      <input type="hidden" name="csrf" value="">
+
+      <div class="col">
+        <div class="input-field">
+          <label for="image-name">Image</label>
+          <input type="file" name="image_name" id="image-name">
+        </div>
+
+        <div class="input-field">
+          <label for="name">Name</label>
+          <input type="text" name="name" id="name" placeholder="Name of the dish">
+        </div>
+
+        <div class="input-field">
+          <label for="description">Description</label>
+          <textarea name="description" id="description" placeholder="Description of the dish" rows="4"></textarea>
+        </div>
+
+        <div class="input-field">
+          <label for="price">Price</label>
+          <input type="text" name="price" id="price" placeholder="Price of the dish">
+        </div>
+
+        <div class="input-field">
+          <label for="category">Category</label>
+          <select name="category" id="category">
+            <option value="" selected disabled>Choose a category</option>
+            <?php foreach ($items as $category): ?>
+              <option value="<?= $category->id ?>"><?= $category->name ?></option>
+            <?php endforeach; ?>
+          </select>
+        </div>
+      </div>
+
+      <div class="col">
+        <div id="image-preview" style="background-image: url('<?= $base_url ?>/assets/img/empty-plate.png');"></div>
+      </div>
+
+      <button class="btn btn-primary">Create</button>
+    </form>
   </div>
 </div>
