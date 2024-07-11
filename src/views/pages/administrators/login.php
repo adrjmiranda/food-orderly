a471ac011979a995cf9b7dfb6b7f9ffc965b0432
Adriano Miranda <adrjmiranda@gmail.com>
2024-07-10 23:48:30 -0300

    chore: created admin login form html structure

--- src/views/pages/administrators/login.php
+++ src/views/pages/administrators/login.php
@@ -9,5 +9,27 @@ $this->extend('administrators/auth', [
 ?>
 
 <div id="auth">
-  <h1>Login</h1>
+  <div class="container">
+    <div class="content">
+
+      <form action="#">
+        <!-- TODO: fill csrf value -->
+        <input type="hidden" name="csrf" value="">
+
+        <h1>Login <i class="fa-solid fa-right-to-bracket"></i></h1>
+
+        <div class="input-field">
+          <label for="email">E-mail:</label>
+          <input type="email" name="email" placeholder="example@email.com">
+        </div>
+
+        <div class="input-field">
+          <label for="password">Password</label>
+          <input type="password" name="password" placeholder="****">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Enter</button>
+      </form>
+    </div>
+  </div>
 </div>
